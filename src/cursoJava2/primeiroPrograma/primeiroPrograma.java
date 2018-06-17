bdc858615b1643c391a91f168a906382628ffde0
Alex <alexsouza8045@gmail.com>
2018-06-17 12:52:33 -0300

    update

---
+++ src/cursoJava2/primeiroPrograma/primeiroPrograma.java
@@ -0,0 +1,23 @@
+package cursoJava2.primeiroPrograma;
+
+/*
+GUANABARA 
+JG CURSOS
+09/06/2018
+ */
+public class primeiroPrograma {
+
+    public static void main(String[] args) {
+
+        double flutuante = 2.56;
+        int inteiro = 10;
+
+        inteiro = (int) 9.87451215;
+
+        String alex = "GUANABARA È O REI";
+
+        alex = String.valueOf(inteiro);
+
+        System.out.println(alex);
+    }
+}
