f91d2bee2789244db6e6030d350d6388f010c42d
guanabaragg <alexsouza8045@gmail.com>
2018-07-15 21:36:07 -0300

    01

--- src/ExercJG/FundamentosProgamacao_Livro/cap5/Pag140Exerc17.java
+++ src/ExercJG/FundamentosProgamacao_Livro/cap5/Pag140Exerc17.java
@@ -1,9 +1,22 @@
 package ExercJG.FundamentosProgamacao_Livro.cap5;
 
+import javax.swing.JOptionPane;
+
 public class Pag140Exerc17 {
 
     public static void main(String[] args) {
 
+        int salCarlos, salJoao, meses = 0;
+
+        salCarlos = Integer.parseInt(JOptionPane.showInputDialog("Informe o salario de Carlos:"));
+
+        salJoao = salCarlos / 3;
         
+        while (salJoao < salCarlos) {
+            salCarlos = salCarlos + (salCarlos * 2 / 100);
+            salJoao = salJoao + (salJoao * 5 / 100);
+            meses = meses + 1;
+        }
+        JOptionPane.showMessageDialog(null, "Meses: " + meses);
     }
 }
--- src/ExercJG/FundamentosProgamacao_Livro/cap5/aula1407.java
+++ src/ExercJG/FundamentosProgamacao_Livro/cap5/aula1407.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ExercJG.FundamentosProgamacao_Livro.cap5;
 
-/**
- *
- * @author alex
- */
+import javax.swing.JOptionPane;
+
 public class aula1407 {
-    
+
+    public static void main(String[] args) {
+
+        
+        String login = "";
+        String senha = "";
+
+        while (!login.equals("alex") && !senha.equals("123")) {
+            for (int i = 1; i < 4; i++) {
+                login = JOptionPane.showInputDialog("Entre com seu login:\nTentativa: " + i);
+                senha = JOptionPane.showInputDialog("Entre com a senha:");
+                if (login.equals("alex") && senha.equals("123")) {
+                    JOptionPane.showMessageDialog(null, "Login e senha corretos", "GUANABARA", 1);
+                    System.exit(0);
+                }
+            }
+            JOptionPane.showMessageDialog(null, "Login e senha incorretos\nTente novamente mais tarde..", "GUANABARA", 0);
+            System.exit(0);
+        }
+    }
 }
